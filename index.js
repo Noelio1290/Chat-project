c22d73195c2e29c672bc1fbb1914a2699bd6016c
noelio1290 <noelio1290@gmail.com>
2026-02-01 00:43:22 -0600

    feat: add server with express

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(express.json()); //Nos permite recibir datos en formato json
+
+//Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("mi servidor esta funcionando!!!")
+});
+
+//Configuracion de puerto
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server corriendo en el puerto ${PORT}`)
+})
 
