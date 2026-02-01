ac3c5130595ac9e1bc7da3ecc8abc9ec67a16a14
noelio1290 <noelio1290@gmail.com>
2026-02-01 01:18:56 -0600

    feat: add conexion MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
@@ -7,6 +8,16 @@ const app = express();
 //Middlewares
 app.use(cors());
 app.use(express.json()); //Nos permite recibir datos en formato json
+
+//Conexion a MongooDB
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("DB Conectada exitosamente con MongoDB Atlas")
+})
+.catch((err) => {
+  console.log("Error en conexion a Mongo DB", err.message)
+})
 
 //Ruta de prueba
 app.get("/", (req, res) => {
@@ -18,5 +29,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server corriendo en el puerto ${PORT}`)
-})
+});
 
