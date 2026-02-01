6d2c15efcd0e69eb5e7f691062dccee5e28ac537
noelio1290 <noelio1290@gmail.com>
2026-02-01 13:09:41 -0600

    feat: logic is added for user login with DB

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const { register } = require("../controllers/userControllers");
+const { register, login } = require("../controllers/userControllers");
 
 //Defino la ruta POST para registro
 router.post("/register", register);
+//Defino la ruta POST para login
+router.post("/login", login);
 
 module.exports = router;
